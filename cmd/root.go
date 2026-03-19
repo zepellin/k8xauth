42cc254ce6c7b94d293a44507050dca9a46b1f69
Martin Danko <martin@nuclia.com>
2026-03-19 17:55:33 +0100

    Update builds with version information

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,18 @@
 package cmd
 
 import (
+	"fmt"
 	"k8xauth/internal/logger"
-
 	"os"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	version string
+	Version   string
+	Commit    string
+	BuildDate string
 )
 
 var RootCmd = &cobra.Command{
@@ -19,7 +22,6 @@ var RootCmd = &cobra.Command{
 authentication of clusters running on different cloud providers or on premise
 without the need to use long-term credentials.`,
 	CompletionOptions: cobra.CompletionOptions{HiddenDefaultCmd: true},
-	Version:           version,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		logLevel, _ := cmd.Flags().GetString("loglevel")
 		logFormat, _ := cmd.Flags().GetString("logformat")
@@ -39,7 +41,28 @@ func Execute() {
 	}
 }
 
+func versionString() string {
+	version := Version
+	if version == "" {
+		version = "dev"
+	}
+	commit := Commit
+	if commit == "" {
+		commit = "unknown"
+	}
+	buildDate := BuildDate
+	if buildDate == "" {
+		buildDate = "unknown"
+	}
+	return fmt.Sprintf(
+		"Version:    %s\nCommit:     %s\nBuild date: %s\nGo version: %s\nOS/Arch:    %s/%s\n",
+		version, commit, buildDate, runtime.Version(), runtime.GOOS, runtime.GOARCH,
+	)
+}
+
 func init() {
+	RootCmd.Version = Version
+	RootCmd.SetVersionTemplate(versionString())
 	RootCmd.PersistentFlags().String("authsource", "all", "Authentication source to use [gke|eks|aks|all] (optional)")
 	RootCmd.PersistentFlags().Bool("printsourceauthtoken", false, "Print source authentication token, useful for debugging. May expose sensitive data")
 	RootCmd.PersistentFlags().String("loglevel", "info", "Set log level [debug|info|warn|error] (optional)")
